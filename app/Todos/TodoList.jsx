fac1fa50d495182488bda41a950d01a5c3f1bc67
Denis Kolomiets <109855341+Denis-Kolomiets@users.noreply.github.com>
2023-01-24 13:03:26 +0200

    Update TodoList.jsx

--- app/Todos/TodoList.jsx
+++ app/Todos/TodoList.jsx
@@ -1,12 +1,13 @@
 import styles from '@/styles/index.module.css'
 import Todo from './Todo'
-const TodoList = ({ todos, deleteTodoHandler }) => {
+const TodoList = ({ todos, deleteTodoHandler, toggleDisabled }) => {
   return (
     <ul className={styles.ul}>
       {todos.length > 0 ? (
         todos.map((todo) => {
           return (
             <Todo
+              toggleDisabled={toggleDisabled}
               key={todo.id}
               todo={todo}
               deleteTodoHandler={deleteTodoHandler}
