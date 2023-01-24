2abc319f3e56f02166ea9184ddc2b8bf1c0d2e03
Denis Kolomiets <109855341+Denis-Kolomiets@users.noreply.github.com>
2023-01-24 13:02:55 +0200

    Update Todo.jsx

--- app/Todos/Todo.jsx
+++ app/Todos/Todo.jsx
@@ -1,10 +1,23 @@
 import styles from '../../styles/index.module.css'
 import { RiDeleteBin2Line } from 'react-icons/ri'
-const Todo = ({ todo, deleteTodoHandler }) => {
+const Todo = ({ todo, deleteTodoHandler, toggleDisabled }) => {
   return (
     <li className={styles.todo} key={todo.id}>
+      <label className={styles.check}>
+        <input type="checkbox" />
+        <span
+          className={styles.checkmark}
+          onClick={() => toggleDisabled(todo.id)}
+        ></span>
+      </label>
       {todo.text}
-      <RiDeleteBin2Line onClick={() => deleteTodoHandler(todo.id)} />
+      <button
+        disabled={!todo.completed}
+        className={styles.btn_del}
+        onClick={() => deleteTodoHandler(todo.id)}
+      >
+        <RiDeleteBin2Line />
+      </button>
     </li>
   )
 }
